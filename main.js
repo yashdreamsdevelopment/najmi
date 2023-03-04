53785b51805ea572a995f8c7db705d5c1192854d
yashwant <yash.mangate1@gmail.com>
2023-03-04 16:17:41 +0530

    added clients section

--- main.js
+++ main.js
@@ -3,3 +3,30 @@ console.log("hello world");
 window.addEventListener("scroll", (e) => {
   console.log(window.pageYOffset);
 });
+
+// client swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // Default parameters
+  slidesPerView: 5,
+  spaceBetween: 10,
+
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
